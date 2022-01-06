02cc890d8c57b1e5ef134043e27d90b561e456d5
Yan Sun <tsun812@gmail.com>
2022-01-06 15:57:38 -0800

    fetch reservations from db with postgreSQL

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,7 +72,9 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query( `SELECT properties.* FROM reservations JOIN properties ON property_id = properties.id WHERE guest_id = $1   LIMIT $2`, [guest_id, limit])
+  .then(res => res.rows)
+  .catch(err => console.log(err));
 }
 exports.getAllReservations = getAllReservations;
 
